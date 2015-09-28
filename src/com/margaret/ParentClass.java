77f73919c1cd5b6fe8eba4c60b6bfc09e1fa39bc
melkinsgit <margaret.elkins@gmail.com>
2015-09-28 13:27:29 -0500

    Added comments

--- src/com/margaret/ParentClass.java
+++ src/com/margaret/ParentClass.java
@@ -7,6 +7,7 @@ public class ParentClass {
     BabyClass OtherNoise = new BabyClass();
     BabyClass NotMuchNoise = new BabyClass();
 
+    // it's cool the way the file name changes blue when it has been edited
     LittleNoise.roar();
     OtherNoise.roar();
     NotMuchNoise.roar();
