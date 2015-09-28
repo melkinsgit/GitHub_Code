fe4ca1b198bada53fd59f716d43b675a34ad4a62
melkinsgit <margaret.elkins@gmail.com>
2015-09-28 13:22:12 -0500

    First v of GitHub

---
+++ src/com/margaret/BabyClass.java
@@ -0,0 +1,12 @@
+package com.margaret;
+
+/**
+ * Created by sn0173nd on 9/28/2015.
+ */
+public class BabyClass {
+
+    public static void roar (){
+
+        System.out.println("grrrowl ... ");
+    }
+}
---
+++ src/com/margaret/ParentClass.java
@@ -0,0 +1,12 @@
+package com.margaret;
+
+public class ParentClass {
+
+    public static void main(String[] args) {
+	BabyClass LittleNoise = new BabyClass();
+    BabyClass OtherNoise = new BabyClass();
+
+    LittleNoise.roar();
+    OtherNoise.roar();
+    }
+}
